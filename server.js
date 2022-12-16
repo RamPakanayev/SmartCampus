124b408b38a5b4b2d44b9a01e9d3c5cba943405b
RamPakanayev <rampakanayev@gmail.com>
2022-12-16 17:54:13 +0200

    commit
    commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/HomePage", (req, res) => {
+  res.sendFile(__dirname + "/HomePage.html");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(5050, () => {
+  console.log("Server listening on port 5050");
+});
