3679d714529e3bb94cc509f3b4f1e40d16dc59c5
RamPakanayev <rampakanayev@gmail.com>
2022-12-19 13:23:12 +0200

    port server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const port = 5050;
 
 app.use(express.static("public"));
 
@@ -11,6 +12,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5050, () => {
-  console.log("Server listening on port 5050");
+app.listen(port, () => {
+  console.log("Server listening on port " + port);
 });
